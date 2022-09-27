fee62896975ece5a7036710baadc2e4aab1619b2
Sierra <kissellogan10@gmail.com>
2022-09-27 14:15:52 -0400

    Added semicolons

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ let countDisc = null;
 client.once('ready', async () => {
     console.log('Ready!');
     countChan = await client.channels.fetch('1007371696212279356');
-    countDisc = await client.channels.fetch('1022161639040102451')
+    countDisc = await client.channels.fetch('1022161639040102451');
 
-    const lastMessageCollection = await countChan.messages.fetch({ limit: 1 })
-    lastMessage = lastMessageCollection.first()
+    const lastMessageCollection = await countChan.messages.fetch({ limit: 1 });
+    lastMessage = lastMessageCollection.first();
 });
 
 client.on("messageCreate", async msg => {
@@ -37,19 +37,19 @@ client.on("messageCreate", async msg => {
                     if (lastMessage.author.username == msg.author.username) {
                         countDisc.send(`${deletedMsg.author}:
                                 > little over eager there, huh buddy? wait for someone else :heart:`
-                        )
+                        );
                         return;
                     }
                     if (msg.content == parseInt(lastMessage.content)) {
                         countDisc.send(`${deletedMsg.author}:
                                 > woah woah woah, that's already taken`
-                        )
+                        );
                         return;
                     }
                     if (parseInt(lastMessage.content)) {
                         countDisc.send(`${deletedMsg.author}:
                                 > oop, not time for that number right now`
-                        )
+                        );
                         return;
                     }
                     console.log(`Deleted message from ${deletedMsg.author.username}`);
@@ -61,7 +61,7 @@ that doesn't look like a number :rage:`);
                 .catch(console.error);
         }
     }
-})
+});
 
 // eslint-disable-next-line no-undef
 client.login(process.env.BOT_TOKEN);
