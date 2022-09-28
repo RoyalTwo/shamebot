9776bc595ada043fc1c7d0d669fd97662f2139a1
Sierra <kissellogan10@gmail.com>
2022-09-27 22:07:59 -0400

    Messages are now randomly selected from a group

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import { Client, GatewayIntentBits } from 'discord.js';
+import fs from 'fs';
 
 const client = new Client({
     intents: [
@@ -11,6 +12,11 @@ const client = new Client({
     ],
 });
 
+const resMessagesFile = fs.readFileSync('./messages.json', 'utf-8');
+const resMessagesObj = JSON.parse(resMessagesFile);
+const resHeader = (resMessagesObj.header).join(' ');
+const resMessages = resMessagesObj.content;
+
 let lastMessage = null;
 let countChan = null;
 let countDisc = null;
@@ -20,12 +26,12 @@ client.once('ready', async () => {
 
     // remove and switch to database
     // hyland server channels
-    /*countChan = await client.channels.fetch('1007371696212279356');
-    countDisc = await client.channels.fetch('1022161639040102451');*/
+    countChan = await client.channels.fetch('1007371696212279356');
+    countDisc = await client.channels.fetch('1022161639040102451');
 
     // testing server channels
-    countChan = await client.channels.fetch('1004202416314515466');
-    countDisc = await client.channels.fetch('1003409641130164234');
+    /* countChan = await client.channels.fetch('1004202416314515466');
+    countDisc = await client.channels.fetch('1003409641130164234'); */
 
     const lastMessageCollection = await countChan.messages.fetch({ limit: 1 });
     lastMessage = lastMessageCollection.first();
@@ -42,34 +48,40 @@ client.on("messageCreate", async msg => {
                 .then(deletedMsg => {
                     // behold the river of if statements
                     if (lastMessage.author.username == msg.author.username) {
-                        countDisc.send(`${deletedMsg.author}
-                        \nlittle over eager there, huh buddy? wait for someone else :heart:`
-                        );
+                        let rand = Math.floor(Math.random() * (resMessages.sameAuthor.length - 1));
+                        countDisc.send(`${transformHeader(deletedMsg)}
+                        ${resMessages.sameAuthor[rand]}`);
                         return;
                     }
                     if (msg.content == parseInt(lastMessage.content)) {
-                        countDisc.send(`${deletedMsg.author}
-                        > ${deletedMsg.content}
-                        \nwoah woah woah, that's already taken`
-                        );
+                        let rand = Math.floor(Math.random() * (resMessages.takenNum.length - 1));
+                        countDisc.send(`${transformHeader(deletedMsg)}
+                        ${resMessages.takenNum[rand]}`);
                         return;
                     }
                     if (parseInt(msg.content)) {
-                        countDisc.send(`${deletedMsg.author}
-                        > ${deletedMsg.content}
-                        \noop, not time for that number right now`
-                        );
+                        let rand = Math.floor(Math.random() * (resMessages.wrongNum.length - 1));
+                        countDisc.send(`${transformHeader(deletedMsg)}
+                        ${resMessages.wrongNum[rand]}`);
                         return;
                     }
+
+                    let rand = Math.floor(Math.random() * (resMessages.notNum.length - 1));
+                    countDisc.send(`${transformHeader(deletedMsg)}
+                    ${resMessages.notNum[rand]}`);
+
                     console.log(`Deleted message from ${deletedMsg.author.username}`);
-                    countDisc.send(`${deletedMsg.author}
-                    > ${deletedMsg.content}
-                    \nthat doesn't look like a number :rage:`);
                 })
                 .catch(console.error);
         }
     }
 });
+
+function transformHeader(msg) {
+    let newString = resHeader.replace('{AUTHOR}', msg.author);
+    newString = newString.replace('{MSG}', msg.content);
+    return newString;
+}
 
 // eslint-disable-next-line no-undef
 client.login(process.env.BOT_TOKEN);
